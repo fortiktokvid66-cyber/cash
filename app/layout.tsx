2f1308138523874c986419ce9b056dc333b0322f
fortiktokvid66-cyber <fortiktokvid66@gmail.com>
2026-01-09 10:16:40 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ReactNode } from 'react';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>{children}</body>
     </html>
-  )
+  );
 }
